508a148c9c76a6c679655e26af63e66ca80e2e4e
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-10-10 18:18:13 +0700

    feat: routes list API and business dashboard route dropdown

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use('/api/schedules', require('./routes/schedules'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/tickets', require('./routes/tickets'));
 app.use('/api/business', require('./routes/business'));
+app.use('/api/routes', require('./routes/routes'));
 
 // 404 handler
 app.use('*', (req, res) => {
